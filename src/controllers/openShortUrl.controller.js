687adbee4522a8cb2940cb51596f4c0d107ffc32
Gabriel Cardozo <gabriel.scardozo7@gmail.com>
2022-12-21 15:51:57 -0300

    feat: redirecionamento à rota encurtada e update da contagem de acessos

--- src/controllers/openShortUrl.controller.js
+++ src/controllers/openShortUrl.controller.js
@@ -4,15 +4,20 @@ export default async function openShortUrlController(req,res) {
     try {
         const { shortUrl } = req.params;
 
-        const urlFinded = await connection.query('SELECT url,"shortUrl",acesses FROM urls WHERE "shortUrl" = $1;', [shortUrl]);
+
+        const urlFinded = await connection.query('SELECT id, url,"shortUrl",acesses FROM urls WHERE "shortUrl" = $1;', [shortUrl]);
 
         if(urlFinded.rows.length === 0) return res.sendStatus(404);
+
+
+
+        const acessQuantity = urlFinded.rows[0].acesses + 1;
+
+        await connection.query('UPDATE urls SET acesses = $1 WHERE id = $2;', [acessQuantity,urlFinded.rows[0].id]);
+
         
-        // res.redirect(urlFinded.rows[0].url);
-
-        res.send(urlFinded.rows);
-
-        // fazer o update da contagem de acessos
+    
+        res.redirect(urlFinded.rows[0].url);
 
     } catch (error) {
         console.log(error);
