b4bd7e1dafc78e0f66fe9145864cc7ae971f87dc
Gabriel Cardozo <gabriel.scardozo7@gmail.com>
2022-12-22 18:07:05 -0300

    refact: retirada array inútil da função

--- src/controllers/ranking.controller.js
+++ src/controllers/ranking.controller.js
@@ -4,10 +4,6 @@ export default async function rankingController(req,res) {
     try {
         const rankingList = await connection.query('SELECT users.id, users.username AS name, COUNT(urls."userId") AS "linksCount", SUM(urls.acesses) AS "visitCount"  FROM users LEFT JOIN urls ON users.id = urls."userId" GROUP BY users.id;');
 
-        const arrOrdened = rankingList.rows.sort((a,b) => {
-            return b.visitCount - a.visitCount;
-        });
-
         res.send(rankingList.rows.slice(0,10));
 
     } catch (error) {
